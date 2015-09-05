f8ddf53d116c0130bc143afe5707038cbe9dff52
limjonghee <caffelatte0522@naver.com>
2015-09-05 11:53:48 +0900

    no message

--- js/user.js
+++ js/user.js
@@ -16,6 +16,10 @@ var user = {
 			this.$el.find('#btnClose').click(function(){
 				user.closeModal();
 			});
+			
+			this.$el.find('#btnSubmit').click(function(){
+				user.signUp();
+			});
 		},
 			this.$el.find('#btnClose').click(function(){
 				this.resetModal();
@@ -28,5 +32,31 @@ var user = {
 		},
 			closeModal : function(){
 				this.$el.find('.signForms').val('');
-		}
+		},
+		
+		signUp : function(){
+			var email = this.$el.find('#inputEmail').val(),
+				password = this.$el.find('#inputPassword').val(),
+				passwordConfirm = this.$el.find('#inputPasswordConfirm').val()
+				name = this.$el.find('#inputName').val(),
+				job = this.$el.find('#inputJob').val();
+			
+		validate : function(){
+		var $signForms = this.$el.find('.signForms'),
+			result = true;
+
+		$.each($signForms, function(index, signForm){
+			var $signForm = $(signForm);
+
+			if(!$signForm.val()){
+				$signForm.addClass('empty');
+				result = false;
+			}else{
+				$signForm.removeClass('empty');
+			}
+		});
+
+		return result;
+	}
+
 }
